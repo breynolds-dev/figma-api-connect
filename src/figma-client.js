d6f72695f0a2e0f04739dbd905579e018294a327
Brian Reynolds <brian.reynolds@equipmentshare.com>
2019-06-29 10:13:56 -0500

    feat(FigmaClient): renamed Figma class to FigmaClient
    Class was renamed to better provide context over what is might actually be doing
    
    BREAKING CHANGE: Figma class can no longer be imported by name, FigmaClient must be used instead

--- src/figma-client.js
+++ src/figma-client.js
@@ -1,31 +1,45 @@
 import axios from "axios";
 
-export class Figma {
+export class FigmaClient {
   constructor( { accessToken = "", apiRoot = "", } ) {
-    const headers = {
+    let headers = {
       "X-FIGMA-TOKEN": accessToken
     };
 
-    const client = axios.create( {
+    this.client = axios.create( {
       baseURL: `https://${apiRoot || "api.figma.com"}/v1/`,
       headers
     } );
+  }
 
-    return {
-      client,
-      file: ( fileId, params = {} ) => client.get( `files/${fileId}`, { params } ),
-      fileImages: ( fileId, params ) =>
-        client.get( `images/${fileId}`, {
-          params: {
-            ...params,
-            ids: params.ids.join( "," )
-          }
-        } ),
-      comments: ( fileId ) => client.get( `files/${fileId}/comments` ),
-      postComment: ( fileId, params ) =>
-        client.post( `files/${fileId}/comments`, params ),
-      teamProjects: ( teamId ) => client.get( `teams/${teamId}/projects` ),
-      projectFiles: ( projectId ) => client.get( `projects/${projectId}/files` )
-    };
+  file( fileId, params = {} ) {
+    this.client.get( `files/${fileId}`, { params } );
+  }
+
+  fileImages( fileId, params ) {
+    this.client.get( `images/${fileId}`, {
+      params: {
+        ...params,
+        ids: params.ids.join( "," )
+      }
+    } );
+  }
+
+  comments( fileId ) {
+    this.client.get( `files/${fileId}/comments` );
+  }
+
+  postComment( fileId, params ) {
+    this.client.post( `files/${fileId}/comments`, params );
+  }
+
+  teamProjects( teamId ) {
+    this.client.get( `teams/${teamId}/projects` );
+  }
+
+  projectFiles( projectId ) {
+    this.client.get( `projects/${projectId}/files` );
   }
 }
+
+export default FigmaClient;
